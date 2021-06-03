4d1046dd5f7062642e732e49192d24ab90b039ff
xhelal-png <82276650+xhelal-png@users.noreply.github.com>
2021-06-03 19:56:18 +0200

    Delete porfolio-2.updated directory

--- porfolio-2.updated/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-function toggle() {
-  navLinks.classList.toggle("show");
-}
-
-hamburgerButton.addEventListener("click", toggle);
